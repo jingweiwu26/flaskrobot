03495fb37ff7eef8f120a8618c8a6cf58b65dda3
jingwei.wu@baruchmail.cuny.edu <jingwei.wu@baruchmail.cuny.edu>
2020-08-23 13:26:22 -0400

    verify

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, make_response
+import hashlib
 import pandas as pd
 import tushare as ts
 
@@ -7,9 +8,26 @@ pro = ts.pro_api('8a23aa12fefee5feeddd696e1d41cdfd8cdf3f257ab79b9a6c2a1bf4')
 app = Flask(__name__)
 
 
+
 @app.route("/")
 def index():
     return "Hello World!"
+
+
+@app.route('/wechat_api/', methods=['GET', 'POST'])
+def wechat_api():
+    if request.method == 'GET':
+        token = 'Empire22'
+        data = request.args
+        signature = data.get('signature', '')
+        timestamp = data.get('timestamp', '')
+        nonce = data.get('nonce', '')
+        echostr = data.get('echostr', '')
+        s = sorted([timestamp, nonce, token])
+        s = ''.join(s)
+        if hashlib.sha1(s.encode('utf-8')).hexdigest() == signature:
+            response = make_response(echostr)
+            return response
 
 
 def get_max_drawdown():
@@ -18,7 +36,7 @@ def get_max_drawdown():
 
 def get_benchmark():
     df = pro.index_daily(ts_code='399300.SZ')
-
+    return df
 
 
 if __name__ == "__main__":
